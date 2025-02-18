9a1c767aef86b7cda9a3b6d16faf8407569bb95b
alfayash <yzyashkumar4@gmail.com>
2025-02-18 22:22:53 +0530

    Added plots and scripts for assignment

---
+++ dataplotting/plot2.R
@@ -0,0 +1,5 @@
+png("plot2.png", width = 480, height = 480)
+plot(subset_data$datetime, subset_data$Global_active_power, type = "l",
+     xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.off()
+
